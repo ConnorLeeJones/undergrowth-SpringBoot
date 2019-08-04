d434b60468cf508ce1d5ba194da6edcfd9aa67f2
Connor Jones <ConnorLeeJones@gmail.com>
2019-08-04 11:02:48 -0400

    Editing profiles working

--- src/main/java/com/undergrowth/undergrowth/services/UserService.java
+++ src/main/java/com/undergrowth/undergrowth/services/UserService.java
@@ -29,7 +29,6 @@ public class UserService {
     public User findUserByUsername(String username){return repository.findUserByUsername(username);}
 
     public User create(User user){
-        //User newUser = new User(user.getUsername(), user.getPassword(), user.getFirstName(), user.getLastName());
         return repository.save(user);
     }
 
