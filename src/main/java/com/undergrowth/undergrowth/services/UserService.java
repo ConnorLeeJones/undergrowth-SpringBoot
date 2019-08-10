b56fc7fb16dcfdac1a58b6483de0b429454e834b
Connor Jones <ConnorLeeJones@gmail.com>
2019-08-10 15:59:17 -0400

    preparing to deploy

--- src/main/java/com/undergrowth/undergrowth/services/UserService.java
+++ src/main/java/com/undergrowth/undergrowth/services/UserService.java
@@ -4,6 +4,7 @@ import com.undergrowth.undergrowth.models.User;
 import com.undergrowth.undergrowth.models.UserProfile;
 import com.undergrowth.undergrowth.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 
@@ -32,17 +33,36 @@ public class UserService {
     public User findUserByUsername(String username){return repository.findUserByUsername(username);}
 
     public User create(User user){
-//        return repository.save(user);
         User newUser = repository.save(user);
         UserProfile userProfile = this.userProfileService.create(new UserProfile());
         userProfile.setUserFullName(newUser.getFirstName() + " " + newUser.getLastName());
         newUser.setUserProfile(userProfile);
+        newUser.setPassword(encryptPassword(user.getPassword()));
         return repository.save(newUser);
     }
-//
-//    public Iterable<User> findAllUsersByProfileType(String type){
-//
-//    }
+
+    public User login(User user){
+        try {
+            User tempUser = findUserByUsername(user.getUsername());
+            if (confirmPassword(user.getPassword(), tempUser.getPassword())){
+                return tempUser;
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+
+    private String encryptPassword(String password) {
+        String salt = BCrypt.gensalt(4);
+        return BCrypt.hashpw(password, salt);
+    }
+
+    private Boolean confirmPassword(String password, String hashed) {
+        return BCrypt.checkpw(password, hashed);
+    }
 
 
 }
